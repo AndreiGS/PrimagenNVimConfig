109277ffd164892b83357b52213871c54090fa91
AndreiGS <andreibv96@yahoo.com>
2023-08-08 17:02:47 +0300

    nice config updated

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -4,3 +4,28 @@ vim.keymap.set("n", "<F5>", function() dap.toggle_breakpoint() end)
 vim.keymap.set("n", "<F9>", function() dap.continue() end)
 vim.keymap.set("n", "<F10>", function() dap.step_over() end)
 vim.keymap.set("n", "<F11>", function() dap.step_into() end)
+
+dap.adapters.lldb = {
+  type = 'executable',
+  command = 'lldb-vscode', -- adjust as needed, must be absolute path
+  name = 'lldb'
+}
+
+dap.configurations.cpp = {
+  {
+    name = 'Launch',
+    type = 'lldb',
+    request = 'launch',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    args = {},
+  },
+}
+
+-- If you want to use this for Rust and C, add something like this:
+
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
